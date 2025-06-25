b26b75de784d3ef82ee9f8e25f97798c2ae1b812
Ankush Yadav <ankushyadav4005@gmail.com>
2025-06-25 21:18:34 +0530

    the sidebar is ready

---
+++ src/components/RightSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RightSidebar = () => {
+  return (
+    <div>
+        <h1>
+            <h1>
+                RightSidebar
+            </h1>
+        </h1>
+    </div>
+  )
+}
+
+export default RightSidebar
